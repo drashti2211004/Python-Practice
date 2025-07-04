03d319446dcd129a94a55f3edf4559ece94ee3bc
drashti <drashtipatel2211004>
2025-07-04 21:16:46 +0530

    demo

---
+++ palindrome_string.py
@@ -0,0 +1,19 @@
+def string_palindrome(s):
+    rev=s[::-1]
+
+    if s==rev:
+        print("palindrome !!")
+    else:
+        print("not palindrome !!")
+
+s=input("Enter name: ")
+string_palindrome(s)
+
+def slicing_mid(s):
+    if len(s)%2==0:
+        print(s)
+    else:
+        mid=len(s)//2
+        print(s[mid-1]+s[mid]+s[mid+1])
+s=input("enter your string: ")
+slicing_mid(s)
---
+++ slicing.py
@@ -0,0 +1,19 @@
+    ## pro
+s= "python is programming langugage"
+print(len(s))
+
+print(s[5:16:2])
+print(s[:18:3])
+print(s[5::4])
+print(s[6:14:2])
+print(s[16:28:3])
+
+   ##negtivity string
+s= "python is programming langugage"
+print(len(s))
+
+print(s[-29:-6:3])
+print(s[-20:-3:1])
+print(s[::-1])
+print(s[:-6:3])
+print(s[-18::4])
